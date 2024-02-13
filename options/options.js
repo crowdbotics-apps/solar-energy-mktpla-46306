82b66af20a4d3f214ec146a3a23d0b4aac16bf14
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:38:03 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-energy-mktpla-46306",
+  url: "https://solar-energy-mktpla-46306.botics.co",
+  api: "https://solar-energy-mktpla-46306.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
